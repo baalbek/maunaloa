b9b860bbf6436d820bc21a59158fc36beadf3fa0
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2013-02-09 17:56:45 +0100

    option for compile before run

--- exec.sh
+++ exec.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # mvn compile
+if [ "$1" = "c" ]; then
+  mvn compile
+fi
 
 mvn exec:java -Dexec.mainClass="maunaloa.App"
-# mvn exec:java -Dexec.mainClass="kalaheo.SpiroGraph"
 
 exit 0
