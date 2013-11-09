4c1dfd5c9760cd6affc293fcf98b8c3dcdbb9bfe
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2013-11-09 13:38:01 +0100

    save ok

--- src/main/clojure/maunaloa/service/mongodb/comments.clj
+++ src/main/clojure/maunaloa/service/mongodb/comments.clj
@@ -1,13 +1,25 @@
 (ns maunaloa.service.mongodb.comments
+  (:import
+    [com.mongodb
+      BasicDBObject
+      DBCollection
+      DBCursor]
+    [org.bson.types ObjectId]
+    [maunaloa.domain MongoDBResult])
   (:require
     (maunaloa.utils [commonutils :as util])
     (maunaloa.service.mongodb [common :as MONGO])))
 
 (defn fetch [^String host
-             ^ObjectId id]
+             ^ObjectId refid]
   (let [coll ^DBCollection (MONGO/get-collection host "comments")
-        query (BasicDBObject. "_id" id)]
+        query (BasicDBObject. "refid" refid)]
     (.toArray ^DBCursor (.find coll query))))
 
 (defn save [^String host
-            ^String comment])
+            ^ObjectId id
+            ^String comment]
+  (let [coll ^DBCollection (MONGO/get-collection host "comments")
+       result (BasicDBObject. "refid" id)]
+    (.append result "c" comment)
+      (MongoDBResult. result (.save coll result))))
