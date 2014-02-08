bf9007182d45f1dd0732114674444586127ceb73
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2014-02-08 21:46:37 +0100

    Removed some more, version 3.0

--- src/main/java/maunaloa/App.java
+++ src/main/java/maunaloa/App.java
@@ -1,14 +1,10 @@
 package maunaloa;
 
-import maunaloa.controllers.DerivativesController;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import maunaloa.controllers.MainFrameController;
-import maunaloa.models.MaunaloaFacade;
 import org.apache.log4j.PropertyConfigurator;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -17,8 +13,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Locale;
 import java.util.Properties;
-
-import javafx.scene.image.Image;
 
 public class App extends Application {
 
@@ -40,25 +34,18 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
         initLog4j();
 
-        //stage.getIcons().setAll(new Image(getClass().getResourceAsStream( "/cmd.ico" ))); 
-        //stage.getIcons().add(new Image(App.class.getResource( "/cmd.png" ).toExternalForm(), 48, 48, false, true));
-        // stage.getIcons().setAll(new Image("file://tmp100/msdos/cmd.png")); 
-        /*
-        stage.getIcons().add(
-                new Image(
-                    JavaFXWindow.class.getResource(
-                        "ui/resources/appicon_48x48.png").toExternalForm(), 48, 48, false, true));
-        */ 
         ApplicationContext factory = new ClassPathXmlApplicationContext(springXmlFileName);
 
         URL url = this.getClass().getResource("/MainFrame.fxml");
 
         FXMLLoader loader = new FXMLLoader(url);
 
+        /*
         final MainFrameController controller = factory.getBean("mainframe-controller",MainFrameController.class);
         controller.setSqldbUrl(getSqldbUrl());
 
         loader.setController(controller);
+        */
 
         Parent parent = (Parent)loader.load();
 
