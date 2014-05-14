56fa9cfd6a83171134a37f45ed55df9d56d06a1a
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2014-05-15 01:42:25 +0200

    DraggableTextArea constructor

--- src/main/java/maunaloa/views/charts/DraggableTextArea.java
+++ src/main/java/maunaloa/views/charts/DraggableTextArea.java
@@ -1,5 +1,8 @@
 package maunaloa.views.charts;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.geometry.Point2D;
 import javafx.scene.control.TextArea;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -19,15 +22,28 @@ public class DraggableTextArea {
     public DraggableTextArea(String text, double x, double y) {
         textArea = new TextArea(text);
         textArea.setPrefWidth(300);
-        /*textArea.setTranslateX(x);
-        textArea.setTranslateY(y);*/
-        anchor = new Circle();
-        anchor.setFill(Color.TRANSPARENT);
-        anchor.setStroke(Color.BLACK);
-        anchor.setRadius(5);
-        anchor.setCenterX(x);
-        anchor.setCenterY(y);
+        anchor = createAnchor(x,y);
         textArea.translateXProperty().bind(anchor.centerXProperty());
         textArea.translateYProperty().bind(anchor.centerYProperty());
     }
+
+    //region Private Methods
+    private Circle createAnchor(double x, double y) {
+        Circle a = new Circle();
+        a.setFill(Color.TRANSPARENT);
+        a.setStroke(Color.BLACK);
+        a.setRadius(5);
+        a.setCenterX(x);
+        a.setCenterY(y);
+
+        final ObjectProperty<Point2D> mousePressPoint = new SimpleObjectProperty<>();
+        a.addEventHandler(event -> {
+            mousePressPoint.set(new Point2D(event.getX(), event.getY()));
+            //onMousePressed();
+            event.consume();
+        });
+        return a;
+    }
+
+    //endregion Private Methods
 }
