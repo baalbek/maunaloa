07150cf741f810c6869799008d1176be340fb50a
rcs <rolf.c.stadheim@gmail.com>
2016-01-17 15:17:59 +0100

    ChartItemRepository, Tuple2 equals/hashCode

--- src/main/java/maunaloa/repository/ChartItemRepository.java
+++ src/main/java/maunaloa/repository/ChartItemRepository.java
@@ -1,9 +1,17 @@
 package maunaloa.repository;
 
+import maunaloa.charts.RiscLines;
+import oahu.financial.Stock;
+import oahux.controllers.ControllerCategory;
+
+import java.util.List;
+import java.util.Optional;
+
 /**
  * Created by rcs on 16.01.16.
  *
  */
-public class ChartItemRepository {
-
+public interface ChartItemRepository {
+    void addRiscLines(Stock stock, ControllerCategory location, RiscLines value);
+    Optional<List<RiscLines>> getRiscLines(Stock stock, ControllerCategory location);
 }
---
+++ src/main/java/maunaloa/repository/DefaultChartItemRepository.java
@@ -0,0 +1,52 @@
+package maunaloa.repository;
+
+import maunaloa.charts.RiscLines;
+import oahu.dto.Tuple2;
+import oahu.financial.Stock;
+import oahux.controllers.ControllerCategory;
+
+import java.util.*;
+
+/**
+ * Created by rcs on 17.01.16.
+ *
+ */
+public class DefaultChartItemRepository implements ChartItemRepository {
+
+    private Map<Tuple2<Stock,ControllerCategory>, List<RiscLines>> riscLines;
+
+    //region Interface ChartItemRepository
+    @Override
+    public void addRiscLines(Stock stock, ControllerCategory location, RiscLines value) {
+        if (riscLines == null) {
+            riscLines = new HashMap<>();
+        }
+        Tuple2<Stock,ControllerCategory> key = new Tuple2<>(stock,location);
+        List<RiscLines> lines = riscLines.get(key);
+        if (lines == null) {
+            lines = new ArrayList<>();
+            riscLines.put(key, lines);
+        }
+        lines.add(value);
+    }
+
+    @Override
+    public Optional<List<RiscLines>> getRiscLines(Stock stock, ControllerCategory location) {
+
+        if (riscLines == null) {
+            return Optional.empty();
+        }
+
+        List<RiscLines> lines = riscLines.get(new Tuple2<>(stock,location));
+
+        if (lines == null) {
+            return Optional.empty();
+        }
+
+        return Optional.of(lines);
+    }
+    //endregion Interface ChartItemRepository
+
+    //region Private Methods
+    //endregion Private Methods
+}
